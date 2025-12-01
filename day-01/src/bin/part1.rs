a7830e672caa8fcfe0bc64da7aa858dc79380f97
Vincent <vincent.hays@nielseniq.com>
2025-12-01 21:29:36 +0100

    clean up

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -8,6 +8,7 @@ fn main() {
                     let n: i32 = line[1..].parse().unwrap();
                     if line.starts_with('L') { -n } else { n }
                 };
+                
                 let cur = (cur + delta).rem_euclid(100);
                 if cur == 0 {
                     count += 1;
