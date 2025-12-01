8759c64ac6728de333a539573f9aa399f9c0b60c
Vincent <vincent.hays@nielseniq.com>
2025-12-01 21:31:47 +0100

    clean up

--- Justfile
+++ Justfile
@@ -1,2 +1,3 @@
 run day part:
+    cargo fmt --manifest-path day-{{day}}/Cargo.toml
     cargo run --manifest-path day-{{day}}/Cargo.toml --bin part{{part}}
--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,19 +1,18 @@
 fn main() {
-    let res =
-        include_str!("input.txt")
-            .lines()
-            .fold((50, 0), |(mut cur, mut count), line| {
-                let delta = {
-                    let n: i32 = line[1..].parse().unwrap();
-                    if line.starts_with('L') { -n } else { n }
-                };
+    let res = include_str!("input.txt")
+        .lines()
+        .fold((50, 0), |(cur, mut count), line| {
+            let delta = {
+                let n: i32 = line[1..].parse().unwrap();
+                if line.starts_with('L') { -n } else { n }
+            };
 
-                let cur = (cur + delta).rem_euclid(100);
-                if cur == 0 {
-                    count += 1;
-                }
-                (cur, count)
-            });
+            let cur = (cur + delta).rem_euclid(100);
+            if cur == 0 {
+                count += 1;
+            }
+            (cur, count)
+        });
 
     dbg!(res);
 }
