5ee5e085f815f7dbab4b38218839654d9e28ad99
Vincent <vincent.hays@nielseniq.com>
2025-12-01 21:29:53 +0100

    clean up

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -2,13 +2,12 @@ fn main() {
     let res =
         include_str!("input.txt")
             .lines()
-            .into_iter()
             .fold((50, 0), |(mut cur, mut count), line| {
                 let delta = {
                     let n: i32 = line[1..].parse().unwrap();
                     if line.starts_with('L') { -n } else { n }
                 };
-                
+
                 let cur = (cur + delta).rem_euclid(100);
                 if cur == 0 {
                     count += 1;
